436bcefc26432b3bd791f2311895f51d86c2c2ee
NishanthSbz <nishanthsbz05@gmail.com>
2024-06-03 12:30:23 +0530

    fully updated to work with docker

--- app.py
+++ app.py
@@ -1,20 +1,57 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_jwt_extended import JWTManager
-from config import Config
-from models import db
-from routes import auth, main
+import os
+import shutil
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from zipfile import ZipFile
+import docker
 
+# Creating docker client
+docker_client = docker.from_env()
+
+UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__name__)), "uploads") 
+HOSTING_PATH = os.path.join(os.path.dirname(os.path.abspath(__name__)), "hosting")
+DOCKER_FILE_PATH = os.path.join(os.path.dirname(os.path.abspath(__name__)), "Dockerfile temp/Dockerfile")
+
+ALLOWED_EXTENSIONS = {"zip"}
+
+from flask import Flask, render_template, request, redirect, url_for
+port = 8080
 app = Flask(__name__)
-app.config.from_object(Config)
 
-db.init_app(app)
-jwt = JWTManager(app)
+@app.route('/')
+def index():
+    return render_template('upload.html')
 
-app.register_blueprint(auth, url_prefix='/auth')
-app.register_blueprint(main, url_prefix='/api')
+@app.route('/', methods=['POST'])
+def upload_file():
+    global port
+
+    # validating the file and sanitizing the filename
+    uploaded_file = request.files['file']
+    file_name = secure_filename(uploaded_file.filename)
+    if uploaded_file.filename != '':
+
+        # Saving it to the system
+        file_path = os.path.join(UPLOAD_FOLDER, file_name)
+        uploaded_file.save(file_path)
+
+        # Extracting the file and saving it to hosting folder
+        dir_name = file_name.split(".")[0]
+        hosting_location = os.path.join(HOSTING_PATH, dir_name)
+        ZipFile(file_path).extractall(hosting_location)
+
+        # copying over the docker file
+        shutil.copyfile(DOCKER_FILE_PATH, os.path.join(hosting_location,"Dockerfile"))
+
+        # running docker build
+        docker_client.images.build(path=hosting_location, tag=dir_name)
+        docker_client.containers.run(dir_name, detach=True, ports={'80/tcp': port})
+        port += 1
+
+        return render_template('output.html', port=port-1)
+
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
+
     app.run(debug=True)
