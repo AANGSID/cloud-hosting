e11360a4426215f88e7cabdc66b9f670415c2f7b
AANGSID <144688479+AANGSID@users.noreply.github.com>
2024-06-03 09:33:35 +0530

    bulk update

--- website_hosting/app.py
+++ website_hosting/app.py
@@ -5,7 +5,15 @@ from config import Config
 from models import db
 from routes import auth, main
 
+import os
+from flask import Flask, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '../uploads'
+ALLOWED_EXTENSIONS = {"pdf"}
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config.from_object(Config)
 
 db.init_app(app)
